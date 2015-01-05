5e8905e4a1820b5f580c9018e4d21464b2a759d0
luiscasco <luis22casco92@gmail.com>
2015-01-05 17:34:23 -0600

    datos modificados del modelo Login

--- application/models/loginm/loginm.php
+++ application/models/loginm/loginm.php
@@ -27,14 +27,14 @@
 			foreach ($query as $key => $datosusuario) {
 				$_SESSION['compania_id']= $datosusuario->usu_com_id;
 				$_SESSION['cus_id']= $datosusuario->cus_id;
-				$_SESSION['cus_nombre']= $datosusuario->cus_nombres;
+				$_SESSION['cus_nombre']= $datosusuario->cus_nombre;
 					//Estas 2 lineas desapareceran por el momento
 				//$_SESSION['usu_email1']=$datosusuario->usu_email;
 				//$_SESSION['usu_email']='ucontrol.emails@gmail.com';
 
 				//$_SESSION['usu_pass_email']=$datosusuario['usu_pass_email'];
 				//$_SESSION['usu_pass_email']='bisa2013';
-				$_SESSION['usu_nombres']=$datosusuario->usu_nombres;
+				//$_SESSION['usu_nombres']=$datosusuario->usu_nombres;
 				$_SESSION['cus_rol_id_fk']=$datosusuario->cus_rol_id_fk;
 				$idrol = $_SESSION['cus_rol_id_fk'];		
 				//Verificamos y agregamos el rol
@@ -45,19 +45,21 @@
 				$this->db->trans_complete();
 				//verificacion completa
 				foreach ($query2 as $key => $datosrol) {
-					$_SESSION['nombrerol'] = $datosrol->rol_nombre;
+					$_SESSION['rol_nombre'] = $datosrol->rol_nombre;
 				}
 
 				//Verificamos lenguaje
-				$sql = "SELECT lng_abrev FROM crm_lng_lenguaje WHERE lng_com_id=$_SESSION[compania_id]";
+				$sql = "SELECT cid_nombre FROM conf_idioma cid INNER JOIN conf_sucursal csu 
+				ON cid.cid_d=csu.csu_cid_id_fk
+				WHERE cid.cid_id=$_SESSION[cco_id]";
 				$this->db->trans_start();
 					$query3 = $this->db->query($sql);
 					$query3 = $query3->result();
 				$this->db->trans_complete();
 				//verificacion completa
 				foreach ($query3 as $key => $datosidioma) {
-					$_SESSION['lng'] = $datosidioma->lng_abrev;
-					$_SESSION['login_user']=$user;
+					$_SESSION['cid_nombre'] = $datosidioma->cid_nombre;
+					$_SESSION['clog_username']=$user;
 					$_SESSION['active']=true;	
 				}
 			}
